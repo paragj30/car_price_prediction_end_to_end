18d19fc2fa5ea4a29977610c07387ea35df865fa
unknown <paragj30@gmail.com>
2024-04-26 21:56:34 +0200

    second.1 commit added setup.py edited

--- setup.py
+++ setup.py
@@ -1,28 +1,31 @@
-from setuptools import setup, find_packages
+from setuptools import find_packages,setup
 from typing import List
+
+HYPEN_E_DOT='-e .'
+
 
 def get_requirements(file_path:str)->List[str]:
     '''
     This function will return the list of requirements
     '''
     requirements = []
-    HYPHEN_E_DOT = '-e .'
 
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [file.replace("\n","") for file in requirements]
         
-        if HYPHEN_E_DOT in requirements:
-            requirements.remove(HYPHEN_E_DOT)
+        if HYPEN_E_DOT in requirements:
+            requirements.remove(HYPEN_E_DOT)
 
     return requirements
 
 
-setup (
+setup(
     name = "Car Price Prediction End to End Data Science",
-    version = 0.0.1,
+    version = "0.0.1",
     author= "Parag Jadhav",
-    author_email= "paragj30@gmail.com"
+    author_email= "paragj30@gmail.com",
     packages= find_packages(),
     install_requires = get_requirements('requirements.txt')    
+
 )
