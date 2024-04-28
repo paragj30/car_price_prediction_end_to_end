758717a3c18561842759182012bcc951a0ad8b74
unknown <paragj30@gmail.com>
2024-04-28 19:03:12 +0200

    add Procfile and updated requirement.txt gunicorn lib

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ def predict_datapoint():
         results=predict_pipeline.predict(pred_df)
         print("after Prediction")
         return render_template('home.html',results=results[0])
-    
+
 
 if __name__=="__main__":
     app.run(host="0.0.0.0",
