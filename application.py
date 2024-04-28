c3ea97381c7840e16852ce7cb62d9a7371c55d75
unknown <paragj30@gmail.com>
2024-04-28 12:24:38 +0200

    'deployment_configuration_AWS'

--- application.py
+++ application.py
@@ -58,25 +58,4 @@ def predict_datapoint():
     
 
 if __name__=="__main__":
-    app.run(host="0.0.0.0", debug=True)        
-
-'''
-if __name__ == "__main__":
-    
-    logging.info('Testing_1: The execution has started')
-    try:
-        data_ingestion = DataIngestion()
-        train_data_path, test_data_path = data_ingestion.initiate_data_ingestion()
-        #data_transformation_config = DataTransformationConfig()
-
-        data_transformation = DataTransformation()
-        train_arr, test_arr, _ = data_transformation.initiate_data_transormation(train_data_path, test_data_path)
-
-        #Model Training
-        model_trainer=ModelTrainer()
-        print(model_trainer.initiate_model_trainer(train_arr,test_arr))
-
-
-    except Exception as e:
-        logging.info('Custom Exception')
-        raise CustomException(e, sys)'''
+    app.run(host="0.0.0.0")        
